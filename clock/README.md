2a9df9af2f943c89bdfcc9885d863db4ec5c1718
blepman <frank@haatetepe.no>
2020-03-31 04:34:08 +0200

    clock

---
+++ clock/README.md
@@ -0,0 +1,2 @@
+clock_24h returns time in 24h format every n interval.
+Defaults to 5 sec interval
---
+++ clock/clock.py
@@ -0,0 +1,15 @@
+from time import strftime, sleep
+def clock_24h(interval: int=5):
+    def _time():
+        while True:
+            try:
+                if int(strftime("%S")) % interval == 0:
+                    print("\r", strftime("%H:%M:%S"), end="")
+                    sleep(interval)
+                else:
+                    print("\r", f"{interval} sec update.", end="")
+                    sleep(0.1)
+            except:
+                print("\r", strftime("%H:%M:%S"), end="")
+                sleep(5)
+    return str(_time())
