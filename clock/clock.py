51506e97024c3775cd05a4f2d270d8b9b5a8b03a
blepman <frank@haatetepe.no>
2020-12-29 05:36:41 +0100

    add shebang

--- clock/clock.py
+++ clock/clock.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from time import strftime, sleep
 def clock_24h(interval: int=5):
     def _time():
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from platform import system as platform
 from subprocess import check_output, DEVNULL, CalledProcessError
 
