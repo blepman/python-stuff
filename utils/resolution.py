92d113546dd31beec4c4182c10ce4cf3b32b82a2
blepman <frank@haatetepe.no>
2020-12-29 17:26:40 +0100

    add get_xy_from_resolution to resolution.py

---
+++ utils/resolution.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python3
+def get_xy_from_resolution(from_xy, from_res, to_res):
+    """ Get new proportional location of coords (x,y) from known coords (x,y) at a specific resolution.
+    :arg from_xy:   list[int, int] coords (x,y) to convert from.
+    :arg from_res:  list[int, int] Resolution (x,y) to convert from.
+    :arg to_res:    list[int, int] Resolution (x,y) to convert to.
+    :returns:       dict[float, float] New coords as key/value. """
+    return {"x": (from_xy[0] / from_res[0]) * to_res[0], "y": (from_xy[1] / from_res[1]) * to_res[1]}
